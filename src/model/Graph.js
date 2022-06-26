2bc5df3e3eaeadfac2e3e3bea95a7f5fd4d577e9
igor-paiva <igorbatistapaiva@outlook.com>
2022-06-26 15:42:05 -0300

    Implements bfs algorithms
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>

--- src/model/Graph.js
+++ src/model/Graph.js
@@ -20,4 +20,72 @@ export default class Graph {
 
     this.nodes[destId].adjList.push(srcId);
   }
+
+  numEdges() {
+    let edges = 0;
+
+    Object.values(this.nodes).forEach((item) => {
+      edges += item.adjList.length;
+    });
+
+    return edges / 2;
+  }
+
+  bfs(callback) {
+    const visited = {};
+
+    Object.keys(this.nodes).forEach((s) => {
+      if (!visited[s]) {
+        this.bfsFromStart(s, visited, callback);
+      }
+    });
+
+    return visited;
+  }
+
+  bfsFromStart(start, visited, callback) {
+    const queue = [start];
+
+    visited[start] = true;
+
+    while (queue.length > 0) {
+      const u = queue.shift();
+
+      this.nodes[u].adjList.forEach((v) => {
+        if (!visited[v]) {
+          visited[v] = true;
+
+          callback(this.nodes[u].node, this.nodes[v].node);
+
+          queue.push(v);
+        }
+      });
+    }
+  }
+
+  bfsFromStartToDest(start, dest) {
+    const visited = {};
+    const queue = [[start]];
+
+    visited[start] = true;
+
+    while (queue.length > 0) {
+      const path = queue.shift();
+      const node = path[path.length - 1];
+
+      if (node === dest) {
+        return path;
+      }
+
+      this.nodes[node].adjList.forEach((v) => {
+        if (!visited[v]) {
+          queue.push([...path, v]);
+
+          visited[v] = true;
+        }
+      });
+    }
+
+    return [];
+  }
 }
