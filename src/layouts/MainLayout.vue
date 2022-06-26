14d15030f56b33f1bbbf10fa9ce90237196ff843
igor-paiva <igorbatistapaiva@outlook.com>
2022-06-26 11:46:56 -0300

    Changes app layout and home page
    Co-authored-by: thiagolopess <thiago.lopes.santos.tls@gmail.com>

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          The Witcher 3 - White Orchard Map
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -28,13 +28,14 @@
         <q-item-label
           header
         >
-          Essential Links
+          Pages
         </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
           :key="link.title"
           v-bind="link"
+          target="_self"
         />
       </q-list>
     </q-drawer>
@@ -51,46 +52,12 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Home',
+    caption: 'Homepage',
+    icon: 'home',
+    link: '/',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
   },
 ];
 
---
+++ src/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page class="flex flex-center">
+    <div style="position: relative">
+      <canvas id="canvas"></canvas>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import canvasBackground from '../assets/images/white_orchard_clean_map.png';
+
+export default defineComponent({
+  name: 'HomePage',
+  mounted() {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = 1280;
+    canvas.height = 1024;
+
+    const background = new Image();
+    background.src = canvasBackground;
+
+    background.onload = () => {
+      ctx.drawImage(background, 0, 0);
+    };
+  },
+  data() {
+    return {
+      canvasWidth: 1280,
+      canvasHeight: 1024,
+    };
+  },
+});
+</script>
